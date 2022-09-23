b628e9cdeef40012337fcfb625c8610eb7f2b561
Andy Deng <denga@bc.edu>
2022-09-23 11:27:44 -0400

    Global Attractions

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let attractions;
+fetch("../assets/attractions.json")
+  .then((response) => response.json())
+  .then((data) => {
+    attractions = data;
+    filterData("all");
+  });
+
+function filterData(category) {
+  let filtered = attractions
+    .filter((att) => att.Category === category || category === "all")
+    .sort((a, b) => a.Vistors > b.Vistors)
+    .slice(0, 5);
+
+  renderBarChart(filtered);
+}
+
+// TODO: Define an event listener for the dropdown menu
+
+let dropdown = document.querySelector("#attraction-category");
+dropdown.addEventListener("change", (e) => {
+  filterData(e.target.value);
+});
